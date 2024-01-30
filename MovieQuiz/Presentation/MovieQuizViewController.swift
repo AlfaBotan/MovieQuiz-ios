8d95dc47eae8dfbbed498a117ec76203db9b73d7
AlfaBotan <ilya.voloshik@gmail.com>
2024-01-30 20:56:01 +0300

    создал аутлеты и экшены на для UI элементов: в экшен кнопок добавили метод отвечающий за окраску равки в нативно онятный цвер для отображения правильности ответа. изменил радиус скругления рамки ImageVIew добавили функционал отображения первого вопроса используя массив моковых данных

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,16 +2,96 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     
-    @IBOutlet weak var noButton: UIButton!
-    @IBOutlet weak var yesButton: UIButton!
+    // массив вопросов
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+    ]
+    
+    // переменная с индексом текущего вопроса, начальное значение 0
+    // (по этому индексу будем искать вопрос в массиве, где индекс первого элемента 0, а не 1)
+    private var currentQuestionIndex = 0
+    // переменная со счётчиком правильных ответов, начальное значение закономерно 0
+    private var correctAnswers = 0
+   
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        yesButton.layer.cornerRadius = 15
+        let currentQuestion = questions[currentQuestionIndex]
+        show(quiz: convert(model: currentQuestion))
     }
+    
+     //метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        var quizStepViewModel = QuizStepViewModel(image: UIImage(named: model.image) ?? UIImage(),
+                                               question: model.text,
+                                               questionNumber: "\(currentQuestionIndex+1)/\(questions.count)")
+
+        return quizStepViewModel
+    }
+    
+    // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        counterLabel.text = step.questionNumber
+        textLabel.text = step.question
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        if isCorrect {
+            imageView.layer.borderColor = UIColor.ypGreen.cgColor
+        } else {
+            imageView.layer.borderColor = UIColor.ypRed.cgColor
+
+        }
+    }
+    
+    @IBAction func yesButtonClicked(_ sender: UIButton) {
+        let answer = true
+        let correctAnswerCurrentQuestion = questions[currentQuestionIndex].correctAnswer
+        showAnswerResult(isCorrect: answer == correctAnswerCurrentQuestion)
+//        currentQuestionIndex += 1
+    }
+    
+    @IBAction func noButtonClicked(_ sender: UIButton) {
+        let answer = false
+        let correctAnswerCurrentQuestion = questions[currentQuestionIndex].correctAnswer
+        showAnswerResult(isCorrect: answer == correctAnswerCurrentQuestion)
+//        currentQuestionIndex += 1
+    }
+    
+    
 }
 
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+// вью модель для состояния "Вопрос показан"
+struct QuizStepViewModel {
+  // картинка с афишей фильма с типом UIImage
+    var image: UIImage
+  // вопрос о рейтинге квиза
+    var question: String
+  // строка с порядковым номером этого вопроса (ex. "1/10")
+    var questionNumber: String
+}
 /*
  Mock-данные
  
